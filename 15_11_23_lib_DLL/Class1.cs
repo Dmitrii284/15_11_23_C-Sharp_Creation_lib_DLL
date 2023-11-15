a917b28ac3f062c148443fdac0a4ef574b64fa72
Roman Solovov <solovov.roma@bk.ru>
2023-11-15 19:35:03 +0500

    Добавьте файлы проекта.

---
+++ 15_11_23_lib_DLL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*
+ Собираем решение DLL Смотри фото!!! Как подключить
+
+
+ */
+namespace _15_11_23_lib_DLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using _15_11_23_lib_DLL;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
